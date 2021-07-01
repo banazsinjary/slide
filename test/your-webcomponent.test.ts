002472eac42038657cfb39215f2227bddc794996
banaz <banazsinjary@gmail.com>
2021-07-01 11:20:52 -0700

    add list test

--- test/your-webcomponent.test.ts
+++ test/your-webcomponent.test.ts
@@ -7,7 +7,7 @@ describe('SlideBar', () => {
   it('has a list tag', async () => {
     const el = await fixture<SlideBar>(html`<slide-bar></slide-bar>>`);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    const list = el.shadowRoot?.querySelector('ul');
+    expect(list).to.exist;
   });
 });
